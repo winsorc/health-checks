cbd0de86282ff16b43f4e817f971303e8a41a638
Winsor <winsc1@pm.me>
2023-09-28 18:29:13 -0500

    Create wrapper function for check_disk_full

--- all_checks.py
+++ all_checks.py
@@ -8,39 +8,31 @@ def check_reboot():
   #Returns True if the computer has pending reboot.
   return os.path.exists("/run/reboot-required")
 
-def check_disk_usage(disk, min_gb, min_percent):
+def check_disk_full(disk, min_gb, min_percent):
   #returns true if there is enough free space, false if not
   du = shutil.disk_usage(disk)
   #calculate percentage of free space
   percent_free = 100 * du.free / du.total
   #calculate how many free gigabytes
   gigabytes_free = du.free /2**30
-  if percent_free < min_percent or gigabytes_free < min_gb:
-    return False
-  return True
-if not check_disk_usage(disk="/",min_gb= 2,min_percent= 10):
-  print("Disk is full")
-  sys.exit(1)
+  if gigabytes_free < min_gb or percent_free < min_percent:
+    return True
+  return False
 
-print("Enough disk space")
-
-def check_cpu_usage(percent):
-  usage = psutil.cpu_percent(1)
-  print("CPU usage %: {}".format(usage))
-  return usage < percent
-
-if not check_cpu_usage(75):
-  print("ERROR! CPU is overloaded")
-else:
-  print("CPU  is okay!")
+def check_root_full():
+  """Returns True if the root parition is full, False otherwise."""
+  return check_disk_full(disk="/", min_gb=2, min_percent=10)
 
 def main():
   if check_reboot():
     print("Pending Reboot.")
     sys.exit(1)
-  print("No reboot pending.")
+  if check_root_full():
+    print("Root partition full.")
+    sys.exit(1)
+
+  print("Everything is ok.")
   sys.exit(0)
 
 main()
-check_disk_usage()
-check_cpu_usage()
+
